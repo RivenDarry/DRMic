99ca9a2e7a36c4194f29604f189ac373ffef4b21
RivenDarry <darrydai@126.com>
2016-06-20 17:01:25 +0800

    Initial Commit

---
+++ DRMic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DRMic
+//
+//  Created by Darry on 16/6/20.
+//  Copyright © 2016年 Darry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DRMic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DRMic
+//
+//  Created by Darry on 16/6/20.
+//  Copyright © 2016年 Darry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
